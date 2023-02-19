961db03bd0ad8fd08df2918a0225f9b323601abb
Diogo França dos Santos <ictxdiogo@gmail.com>
2023-02-18 22:33:13 -0400

    more fix

--- src/components/index/main.tsx
+++ src/components/index/main.tsx
@@ -8,10 +8,12 @@ interface MainProps {
 export default function Main({ works }: MainProps) {
   return (
     <>
-      <div className=" text-center ">
-        <h1 className="py-14 text-4xl">{`Hi there, i'm Diogo`}</h1>
-        <h2 className=" text-xl">A Full Stack lover</h2>
-        <h3 className="text-base">Take a look at my personal projects</h3>
+      <div className="mb-4 text-center">
+        <h1 className="py-14 text-4xl sm:text-7xl">{`Hi there, i'm Diogo`}</h1>
+        <h2 className=" text-xl sm:text-2xl">A Full Stack lover</h2>
+        <h3 className="text-base sm:text-xl">
+          Take a look at my personal projects
+        </h3>
       </div>
       <WorkGrid work={works} />
     </>
--- src/components/index/workGrid.tsx
+++ src/components/index/workGrid.tsx
@@ -12,12 +12,12 @@ export default function WorkGrid({ work }: WorkGridProps) {
         <Link
           href={item.href}
           key={item.id}
-          className="rounded-lg bg-blue-600 p-6 shadow-md hover:bg-blue-700"
+          className="rounded-lg bg-blue-600 p-4 shadow-md hover:bg-blue-700"
         >
           <h2 className="mb-2 text-lg font-semibold">
             {item.title.toUpperCase()}
           </h2>
-          <p className="mb-4 text-justify ">{item.description}</p>
+          <p className="text-justify">{item.description}</p>
         </Link>
       ))}
     </div>
