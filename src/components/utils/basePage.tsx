f410a499dac1778e4d708c1fbac53106a9abcf34
Diogo França dos Santos <ictxdiogo@gmail.com>
2023-02-28 15:19:38 -0400

    add a title to base page

--- src/components/utils/basePage.tsx
+++ src/components/utils/basePage.tsx
@@ -1,13 +1,21 @@
+import Head from "next/head";
+
 interface BasePageProps {
   children: React.ReactNode;
+  title: string;
 }
 
-export default function BasePage({ children }: BasePageProps) {
+export default function BasePage({ children, title }: BasePageProps) {
   return (
-    <div className="bg-gray-800 text-center text-white transition-all">
-      <div className="flex min-h-screen w-full justify-center">
-        <div className="flex justify-center px-2 sm:w-3/4">{children}</div>
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div className="bg-gray-800 text-center text-white transition-all">
+        <div className="flex min-h-screen w-full justify-center">
+          <div className="flex justify-center px-2 sm:w-3/4">{children}</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
